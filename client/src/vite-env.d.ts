0094861904f2f8cb0065be922d73a1025f5c0fc4
Nolan Bridges <nolanmbridges@gmail.com>
2025-08-16 17:14:11 -0700

    try

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,9 +1,11 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_ACCOUNT_URL: string
+  readonly VITE_ENABLE_LOGGER: string;
+  readonly VITE_LOGGER_FILTER: string;
+  // Add other env variables here
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv
+  readonly env: ImportMetaEnv;
 }
